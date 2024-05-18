2f5832618da01a204e24cd36b7f0932d803bff21
astaaa48 <164441516+astaaa48@users.noreply.github.com>
2024-05-18 13:13:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 image_path="$HOME/z01-settings/Background.png"
 git clone https://github.com/astaaa48/z01-settings ~/z01-settings && cd ~/z01-settings/ && mv .p10k.zsh ~/.p10k.zsh 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 zsh git_setup.sh
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}" && gsettings set org.gnome.desktop.background picture-uri "file://${image_path}"
